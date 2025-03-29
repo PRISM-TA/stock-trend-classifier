406bd0a971e0bb0e5bc75f11a1a87b19736e47bf
root <1155173751@link.cuhk.edu.hk>
2025-03-29 12:04:28 +0800

    Add MLPv3 v4

--- classifiers/MLPClassifier_v3.py
+++ classifiers/MLPClassifier_v3.py
@@ -1,46 +1,42 @@
-# Deeper MLP
+# Deeper MLP with hourglass structure
 from classifiers.BaseClassifier import BaseClassifier
 import torch.nn as nn
 
 
-class MLPClassifier_V2(BaseClassifier):
-    def __init__(self, input_size: int, output_size: int = 3, hidden_size: int = 128, dropout_rate: float = 0.2):
+class MLPClassifier_V3(BaseClassifier):
+    def __init__(self, input_size: int, output_size: int = 3, dropout_rate: float = 0.2):
         super().__init__()
-        self.model_name = "MLPv2"
+        self.model_name = "MLPv3"
 
         self.model = nn.Sequential(
-            # Layer 1
-            nn.Linear(input_size, hidden_size),
+            # Expanding path
+            nn.Linear(input_size, 128),
             nn.ReLU(),
             nn.Dropout(dropout_rate),
             
-            # Layer 2
-            nn.Linear(hidden_size, hidden_size),
+            nn.Linear(128, 256),
             nn.ReLU(),
             nn.Dropout(dropout_rate),
             
-            # Layer 3
-            nn.Linear(hidden_size, hidden_size),
+            nn.Linear(256, 512),  # Widest part
             nn.ReLU(),
             nn.Dropout(dropout_rate),
             
-            # Layer 4
-            nn.Linear(hidden_size, hidden_size),
+            # Contracting path
+            nn.Linear(512, 256),
             nn.ReLU(),
             nn.Dropout(dropout_rate),
             
-            # Layer 5
-            nn.Linear(hidden_size, hidden_size),
+            nn.Linear(256, 128),
             nn.ReLU(),
             nn.Dropout(dropout_rate),
             
-            # Layer 6
-            nn.Linear(hidden_size, hidden_size),
+            nn.Linear(128, 64),
             nn.ReLU(),
             nn.Dropout(dropout_rate),
             
             # Output layer
-            nn.Linear(hidden_size, output_size)
+            nn.Linear(64, output_size)
         )
     
     def forward(self, x):
